445246e93a301899bcd4343331805827b07c529f
Anbu302004 <anbunagarathinam30@gmail.com>
2025-10-13 20:41:55 +0530

    Added new Java programs

--- ReverseStringArray.java
+++ ReverseStringArray.java
@@ -6,16 +6,30 @@ public class ReverseStringArray {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of String:");
         int n = sc.nextInt();
-        String arr[] = new String[n];
-        System.out.println("Enter the String:");
+        sc.nextLine();
+        String arr[] = new String[n];   
+        System.out.println("Enter the Strings one by one :");
         for(int i = 0; i<n; i++){
-            arr[i] = sc.nextLine();     
+            System.err.println("String " + (i+1) + ":"); //print sting line by line
+            arr[i] = sc.nextLine().trim();  // Used trim to remvoe spaces before and after the string    
         }
          System.out.println("\nReverse String Array:");
     for(int i = n-1; i>=0; i--){
         System.out.println(arr[i] + " ");
     }
     System.out.println("\nArray in String format: " + Arrays.toString(arr));
+    String rev[] = new String[n];
+    // for(int i = 0; i<n; i++){
+    //     rev[i] = arr[n-1-i];  
+    // }   this will also work but created a new array
+    for (int i = 0; i < n / 2; i++) {
+    String temp = arr[i];
+    arr[i] = arr[n - 1 - i];
+    arr[n - 1 - i] = temp;
+}
+rev = arr;
+    System.out.println("\nReversed Array in String format:" + Arrays.toString(rev));
+     
     sc.close();
 
     }
