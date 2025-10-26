508527ff1bd1dcdb6d364b4d1fe1ffa625a06246
Anbu302004 <anbunagarathinam30@gmail.com>
2025-10-26 18:15:04 +0530

    Updated WaitingList Final

--- SimpleBookingWaitingList.java
+++ SimpleBookingWaitingList.java
@@ -1,91 +1,130 @@
 import java.util.*;
 
-class Ticket{
+class Ticket {
     int seatNo;
     boolean isBooked;
     String passengerName;
 
-    Ticket(int seatNo){
+    Ticket(int seatNo) {
         this.seatNo = seatNo;
         this.isBooked = false;
         this.passengerName = "";
     }
 }
+ 
+class WaitingList {
+    String[] list;
+    int count;
 
-public class SimpleBookingWaitingList{
-    public static void main(String[] args){
+    WaitingList(int size) {
+        list = new String[size];
+        count = 0;
+    }
+
+    boolean add(String name) {
+        if (count >= list.length) {
+            return false;
+        }
+        list[count++] = name;
+        return true;
+    }
+
+    String remove() {
+        if (count == 0) return null;
+        String first = list[0];
+        for (int i = 0; i < count - 1; i++) {
+            list[i] = list[i + 1];
+        }
+        list[--count] = null;
+        return first;
+    }
+
+    void show() {
+        if (count == 0) {
+            System.out.println("No one in the waiting list.");
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println((i + 1) + ". " + list[i]);
+        }
+    }
+
+    boolean isEmpty() {
+        return count == 0;
+    }
+
+    int size() {
+        return count;
+    }
+}
+
+public class SimpleBookingWaitingList {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Ticket[] tickets = new Ticket[10];
         int nextAvailable = 0;
-        String[] waitingList = new String[5];
-        int waitingCount = 0;
+        WaitingList waitingList = new WaitingList(5);
 
-        for(int i=0; i<tickets.length; i++){
-            tickets[i] = new Ticket(i+1);
+        for (int i = 0; i < tickets.length; i++) {
+            tickets[i] = new Ticket(i + 1);
         }
 
-        while(true){
-            System.out.println("1. Book Ticket");
-            System.out.println("2. Show available Tickets");
-            System.out.println("3. Cancel Booking:");
+        while (true) {
+            System.out.println("\n1. Book Ticket");
+            System.out.println("2. Show Available Tickets");
+            System.out.println("3. Cancel Booking");
             System.out.println("4. Show Waiting List");
             System.out.println("5. Exit");
-            System.out.println("Enter your choice:");
+            System.out.print("Enter your choice: ");
             int choice = sc.nextInt();
             sc.nextLine();
 
-            switch(choice){
+            switch (choice) {
                 case 1:
                     System.out.print("Enter your Name: ");
                     String name = sc.nextLine();
-                    System.out.print("Enter number of Tickets to book:");
+                    System.out.print("Enter number of Tickets to book: ");
                     int n = sc.nextInt();
                     sc.nextLine();
-                    int bookedCount = 0; 
+                    int bookedCount = 0;
 
-                    // Book available tickets first
-                    for(int i=0; i<n; i++){
-                        if(nextAvailable >= tickets.length){
-                            break;
-                        }
+                    for (int i = 0; i < n; i++) {
+                        if (nextAvailable >= tickets.length) break;
+
                         Ticket t = tickets[nextAvailable];
-                        if(!t.isBooked){
+                        if (!t.isBooked) {
                             t.isBooked = true;
                             t.passengerName = name;
                             bookedCount++;
-                            System.out.println("Ticket No "+t.seatNo+ " booked Successfully for "+name);
+                            System.out.println("Ticket No " + t.seatNo + " booked successfully for " + name);
                             nextAvailable++;
                         }
                     }
 
-                    // Add remaining to waiting list
-                    if(bookedCount < n){
+                    if (bookedCount < n) {
                         int toWait = n - bookedCount;
-                        for(int i=0; i<toWait && waitingCount < waitingList.length; i++){
-                            waitingList[waitingCount++] = name;
-                            System.out.println(name+" added to waiting list (Position "+waitingCount+")");
-                        }
-
-                        if(waitingCount == waitingList.length && bookedCount + toWait > tickets.length){
-                            System.out.println("Waiting list is full. Cannot add more.");
+                        for (int i = 0; i < toWait; i++) {
+                            if (waitingList.add(name)) {
+                                System.out.println(name + " added to waiting list (Position " + waitingList.size() + ")");
+                            } else {
+                                System.out.println("Waiting list is full. Cannot add more.");
+                                break;
+                            }
                         }
                     }
-
                     break;
 
                 case 2:
                     System.out.println("Available Tickets:");
                     boolean anyAvailable = false;
-                    for(Ticket t : tickets){
-                        if(!t.isBooked){
-                            System.out.println("Ticket No:" +t.seatNo);
+                    for (Ticket t : tickets) {
+                        if (!t.isBooked) {
+                            System.out.println("Ticket No: " + t.seatNo);
                             anyAvailable = true;
                         }
                     }
-                    if(!anyAvailable){
+                    if (!anyAvailable) {
                         System.out.println("No tickets available.");
-                    }else{
-                        System.out.println("All tickets are booked.");
                     }
                     break;
 
@@ -102,22 +141,13 @@ public class SimpleBookingWaitingList{
                                 System.out.println("Booking for Ticket " + t.seatNo + " cancelled successfully.");
                                 t.isBooked = false;
                                 t.passengerName = "";
-
-                                // Assign ticket to first waiting person if any
-                                if(waitingCount > 0){
-                                    String nextPerson = waitingList[0];
+ 
+                                if (!waitingList.isEmpty()) {
+                                    String nextPerson = waitingList.remove();
                                     t.isBooked = true;
                                     t.passengerName = nextPerson;
                                     System.out.println("Ticket " + t.seatNo + " automatically assigned to " + nextPerson);
-
-                                    // Shift waiting list forward
-                                    for(int j=0; j<waitingCount-1; j++){
-                                        waitingList[j] = waitingList[j+1];
-                                    }
-                                    waitingList[waitingCount-1] = null;
-                                    waitingCount--;
                                 }
-
                             } else {
                                 System.out.println("Ticket " + t.seatNo + " is not booked.");
                             }
@@ -128,21 +158,13 @@ public class SimpleBookingWaitingList{
                     break;
 
                 case 4:
-                    System.out.println("Waiting List");
-                    if(waitingCount == 0){
-                        System.out.println("No one in the waiting List.");
-                    }else{
-                        int pos = 1;
-                        for(int i=0; i<waitingCount; i++){
-                            System.out.println(pos+" . "+waitingList[i]);
-                            pos++;
-                        }
-                    } 
-                    break;  
+                    System.out.println("Waiting List:");
+                    waitingList.show();
+                    break;
 
                 case 5:
                     System.out.println("Exiting...");
-                    sc.close(); 
+                    sc.close();
                     return;
 
                 default:
