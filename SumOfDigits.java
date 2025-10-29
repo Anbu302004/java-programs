59709cdb696487c5e09e6c0cfeda64c26d63f2b6
Anbu302004 <anbunagarathinam30@gmail.com>
2025-10-29 19:35:12 +0530

    Added new java programs

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -4,7 +4,7 @@ public class SumOfDigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Enter a Value A:");
+        System.out.println("Enter a Value:");
         int val = sc.nextInt();
         
         int sumOfDigits = 0;  
