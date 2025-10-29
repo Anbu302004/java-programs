a382ed9bae95875773dfc0cc19fc59eb2a46240b
Anbu302004 <anbunagarathinam30@gmail.com>
2025-10-29 19:31:47 +0530

    Sum of Digit and Leap year check Added

---
+++ SumOfDigits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a Value A:");
+        int val = sc.nextInt();
+        
+        int sumOfDigits = 0;  
+        int mainval = val;  
+         
+        while(mainval != 0){
+            sumOfDigits += mainval % 10; 
+            mainval /= 10; 
+        } 
+        System.out.println("Sum of the Digits is: " + sumOfDigits);
+        
+        sc.close();
+    }
+}
